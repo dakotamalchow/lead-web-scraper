b86d3fa48b6ecd4ee17e74ba3d3f652c7f06de5c
Dakota Malchow <dakotamalchow@gmail.com>
2023-03-13 23:56:12 -0600

    Add checkboxes for the lead types

--- client/src/app.js
+++ client/src/app.js
@@ -3,6 +3,8 @@ import './app.css';
 
 function App() {
   const [data, setData] = React.useState(null);
+  const [propertyManagers, setPropertyManagers] = React.useState(false);
+  const [realEstateAgents, setRealEstateAgents] = React.useState(false);
   const [zipCodes, setZipCodes] = React.useState("");
 
   const handleSubmit = (event) => {
@@ -12,12 +14,29 @@ function App() {
       .then((data) => setData(data));
   }
 
+  const handlePropertyManagers = (event) => {
+    setPropertyManagers(event.target.value);
+  }
+
+  const handleRealEstateAgents = (event) => {
+    setRealEstateAgents(event.target.value);
+  }
+
+  const handleZipCodes = (event) => {
+    setZipCodes(event.target.value);
+  }
+
   return (
     <div>
+      <h1>Lead Generator</h1>
       <form onSubmit={handleSubmit}>
+        <input type="checkbox" id="propertyManagers" name="propertyManagers" value={propertyManagers} onChange={handlePropertyManagers}></input>
+        <label htmlFor="propertyManagers">Property Managers</label>
+        <input type="checkbox" id="realEstateAgents" name="realEstateAgents" value={realEstateAgents} onChange={handleRealEstateAgents}></input>
+        <label htmlFor="realEstateAgents">Real Estate Agents</label>
         <label>
           Zip Codes:
-          <input type="text" name="zipCodes" value={zipCodes} onChange={(event) => setZipCodes(event.target.value)} />
+          <input type="text" name="zipCodes" value={zipCodes} onChange={handleZipCodes} />
         </label>
         <input type="submit" />
       </form>
